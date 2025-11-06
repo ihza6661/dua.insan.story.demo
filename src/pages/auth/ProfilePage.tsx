6d217fb2ebd39f5a73895437aac049ca2777be00
ihza6661 <ihzahmahendra6661@gmail.com>
2025-11-06 14:50:15 +0700

    remove bg

--- src/pages/auth/ProfilePage.tsx
+++ src/pages/auth/ProfilePage.tsx
@@ -158,7 +158,7 @@ const ProfilePage: FC = () => {
   }
 
   return (
-    <div className="container mx-auto py-20 bg-background">
+    <div className="container mx-auto py-32">
       <h1 className="text-3xl font-bold mb-8 text-foreground">Profil Saya</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
         <Card>
